1e75df2c107bea250454a891c79d692caf85f9f6
Lev <lev01111989@gmail.com>
2025-09-01 10:04:19 +0200

    Attempt2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import io
+from io import BytesIO
 
 from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
@@ -13,7 +13,7 @@ def serialize_car_object(car: Car) -> bytes:
 
 
 def deserialize_car_object(json_bytes: bytes) -> Car:
-    stream = io.BytesIO(json_bytes)
+    stream = BytesIO(json_bytes)
     data = JSONParser().parse(stream)
     serializer = CarSerializer(data=data)
     serializer.is_valid(raise_exception=True)
